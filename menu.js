be3f288ecd825058870c8123a46d46ef91164523
Krishna Ghose <krishna.ghose.eu@gmail.com>
2021-02-09 04:11:51 +1100

    Updated assignement five

--- menu.js
+++ menu.js
@@ -4,50 +4,62 @@ const getMealData = meal => {
     const url = `${apiBase}?f=${meal}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => displayAllMenu(data.meals));                
+        .then(data => displayAllMenu(data));                
 }
 
 const searchBtn = document.getElementById('search_button');
 searchBtn.addEventListener('click', () => {    
-    let inputCity = document.getElementById('meal').value;
-    getMealData(inputCity);               
+    let inputMenu = document.getElementById('meal').value;
+    getMealData(inputMenu);               
 })
 
 
-const displayAllMenu = allMenu =>{ 
-    document.getElementById('all-menu').innerHTML = "";   
+const displayAllMenu = allMenu =>{        
     const allMenuDiv = document.getElementById('all-menu');
-    allMenu.forEach(singleMenu => {
+    allMenuDiv.innerHTML = '';
+    allMenu.meals.forEach(singleMenu => {
+        const {strMeal, strMealThumb} = singleMenu;
         const singleMenuDiv = document.createElement('div');
         singleMenuDiv.className = 'single-menu';
-        const menuInfo = `
-        <img class="menu-img rounded" src="${singleMenu.strMealThumb}">
-        <h5 class="menu-name text-center">${singleMenu.strMeal}</h5>
-        
-        `;
-        singleMenuDiv.innerHTML = menuInfo;       
-        allMenuDiv.appendChild(singleMenuDiv);  
-                                            
-        // singleMenuDiv.onclick="displaySingleMenu()";      
+        singleMenuDiv.innerHTML = `
+        <div class="menu-display" onclick="getIngredientsData('${strMeal}')">
+        <img class="menu-img rounded" src="${strMealThumb}">
+        <h5 class="menu-name text-center">${strMeal}</h5> 
+        </div>         
+        `;     
+        allMenuDiv.appendChild(singleMenuDiv);
+
     });
 }
 
-/* <button onclick="displaySingleMenu('${singleMenu.strMeal}')">Details</button> */
-
-const displaySingleMenu = singleMenu => {
-    const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=list`;
+const getIngredientsData = menuName =>{
+    const url = `${apiBase}?s=${menuName}`;
     fetch(url)
-    .then(response => response.json())
-    .then(data => displayAllMenu(data.meals));  
+        .then(response => response.json())
+        .then(data => displayMenuIngredients(data.meals[0]));
 }
 
-
-const renderMenuInfo = menu =>{
-    const menuDiv = document.getElementById('menuDetail');
-    menuDiv.innerHTML = `
-    <h1>${singleMenu.strMeal}</h1>
-    <p>${singleMenu.strIngredient}</p>
-    <img src="${singleMenu.strMealThumb}">
-    `
+const displayMenuIngredients = getMenu => {
+    const {strMeal, strMealThumb} = getMenu;
+    const menuIngredients = document.getElementById('ingredient-container');
+    menuIngredients.innerHTML = `
+    <div class="meal-ingredients">
+    <img class="menu-img rounded" src="${strMealThumb}">
+    <div class="ingredients">
+    <h3>${strMeal}</h3>
+    <h5>List of Ingredients</h5>
+    <ul>${listMenuIngredients(getMenu)}</ul>
+    </div>
+    </div>
+    `;
 }
+
+const listMenuIngredients = list => {
+    let ingredients = '';
+    for (let i = 1; i <= 10; i++) {        
+       ingredients+= `<li>${list["strIngredient" + i]}</li>`;
+    }   
+    return ingredients;    
+}
+
 
